9bcf7926229aff47bc4141ea3d7ed56a4e5d5f3d
Vitor Hugo Banaco <vitor_psytk@hotmail.com>
2024-03-06 23:36:57 -0300

    Atualização de Novas aulas

---
+++ curso_python_udemy/aula24_exercicios.py
@@ -0,0 +1,15 @@
+#Varíavel
+nome = 'Vitor'
+sobrenome = 'Banaco'
+idade = 29
+ano_nascimento = 1994     #Nesta varíavel pode se usar também o ano atual menos a varíavel idade (2024 - idade)
+maior_de_idade = idade >18
+altura_metros = 1.71
+
+#Utilização da Varíavel
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
---
+++ curso_python_udemy/aula26.py
@@ -0,0 +1,26 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 3  # Nesta divisçao com uma / o resultado sempre será um número com ponto flutuante 'float'
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 3  # Nesta divisçao com uma // o resultado sempre será um número inteiro 'int'
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10  #potenciação (elevação de um número a outro)
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # Nesta conta sem será o resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)  #Nesta conta ele pergunta se 10 é divisil por 8 se o resultado for 0 ele da o resultado de True, se não for da False.
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
