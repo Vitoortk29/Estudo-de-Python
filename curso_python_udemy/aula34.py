4a7612e97ded11c9a8f04c1608dfbd11d84ff338
Vitor Hugo Banaco <vitor_psytk@hotmail.com>
2024-03-09 16:24:59 -0300

    Adicionei aulas estudas

---
+++ curso_python_udemy/aula34.py
@@ -0,0 +1,9 @@
+entrada = input('Você quer "entar" ou "sair"?')
+
+if entrada == 'entrar':
+        print('Você entrou no Sistema')
+elif entrada == 'sair':
+    print('Você saiu do Sistema')
+else:
+      print('Você não digitou nem entrar e nem sair.')
+      
