1d9a9501a4faeca477b4a6d5a5766478df39fb6e
Vitor Hugo Banaco <vitor_psytk@hotmail.com>
2024-03-12 00:20:28 -0300

    Adicionei aulas

--- curso_python_udemy/aula26.py
+++ curso_python_udemy/aula26.py
@@ -7,16 +7,16 @@ print('Subtração', subtracao)
 multiplicacao = 10 * 10
 print('Multiplicação', multiplicacao)
 
-divisao = 10 / 3  # Nesta divisçao com uma / o resultado sempre será um número com ponto flutuante 'float'
+divisao = 10 / 3  # Nesta divisão com uma / o resultado sempre será um número com ponto flutuante 'float'
 print('Divisão', divisao)
 
-divisao_inteira = 10 // 3  # Nesta divisçao com uma // o resultado sempre será um número inteiro 'int'
+divisao_inteira = 10 // 3  # Nesta divisão com duas // o resultado sempre será um número inteiro 'int'
 print('Divisão inteira', divisao_inteira)
 
 exponenciacao = 2 ** 10  #potenciação (elevação de um número a outro)
 print('Exponenciação', exponenciacao)
 
-modulo = 55 % 2  # Nesta conta sem será o resto da divisão
+modulo = 55 % 2  # Nesta conta sempre será o resto da divisão
 print('Módulo', modulo)
 
 print(10 % 8 == 0)  #Nesta conta ele pergunta se 10 é divisil por 8 se o resultado for 0 ele da o resultado de True, se não for da False.
---
+++ curso_python_udemy/aula41.py
@@ -0,0 +1,18 @@
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+
+senha_permitida = '12345'
+
+
+if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair')
+
+#Avaliação de curto circuito:
+senha = 0 or False or 0 or 'abc' or True
+print(senha)
+
+#Outra opção
+senha = input('Senha: ') or 'Sem senha'
+print(senha)
